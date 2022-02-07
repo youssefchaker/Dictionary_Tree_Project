96d54727271f7f93dd9b2c543c29e297abee7a3e
zeineuub <ghada.malek12@mail.com>
2022-02-07 12:30:32 -0500

    few change

--- dico.h
+++ dico.h
@@ -7,10 +7,10 @@ int nombreAleatoire(int nombreMax);
 void dicoAfficher(TArbre* a,char buffer[], int *cur);
 int dicoNbMotsDifferents(TArbre* a);
 int dicoNbMotsTotal(TArbre* a);
-void dicoInsererMot(char mot[], TArbre* a);
+void dicoInsererMot(char mot[], TArbre** a);
 int dicoNbOcc(char mot[], TArbre* a); 
 TArbre* dicoInsererNouvMot(char mot[], TArbre* a);
 TArbre* dicoBranche(char *mot);
-void dicoNbOccAdd(char *mot, TArbre* a);
+void dicoNbOccAdd(char *mot, TArbre** a);
 void aff(TArbre* a);
 #endif
